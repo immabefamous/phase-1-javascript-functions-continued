922771a190d3a1db95b7b4c79e8f9e8d7d4f1d78
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-16 18:18:46 -0400

    commit tree 3bbb9d51f2e1792338e2c4fd6ede5cede52d1d4a

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity == undefined) {
+        return 'This Saturday, I want to roller-skate!'
+    }
+    else {
+        return 'This Saturday, I want to bathe my dog!'
+    }
+}
+saturdayFun()
+
+function mondayWork(activity) {
+    if (activity == undefined) {
+        return 'This Monday, I will go to the office.'
+    }
+    else {
+        return 'This Monday, I will work from home.'
+    }
+}
+mondayWork()
+
+function wrapAdjective(str) {
+    if (str == '*') {
+        return function result () {
+           return "You are *a hard worker*!"
+        }
+    }
+     else if (str == '||') {
+            return function result () {
+                return "You are ||a dedicated programmer||!"
+            }
+        }
+}
+wrapAdjective()
